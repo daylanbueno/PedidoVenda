d31c86cd0559de15c514b8119ba8c91e7e3902c5
daylan <daylansantos@gmail.com>
2016-11-24 18:43:23 -0200

    ENDERECO DE ENTREGA

--- src/main/java/com/algaworks/pedidovenda/model/EnderecoEntrega.java
+++ src/main/java/com/algaworks/pedidovenda/model/EnderecoEntrega.java
@@ -11,6 +11,7 @@ public class EnderecoEntrega implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	
 	private String logradouro;
 	private String numero;
 	private String complemento;
